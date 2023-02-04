0bea9c69824a64b16f7209cc32e44f06f2de6acd
Sylwia <g.sylwia@vp.pl>
2023-02-04 22:08:10 +0100

    task form create

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -11,4 +11,8 @@ export class TaskService {
   getAll(): Observable<TasksModel[]> {
     return this._httpClient.get<TasksModel[]>('https://63761992b5f0e1eb850298da.mockapi.io/tasks');
   }
+
+  create(task: TasksModel): Observable<void> {
+    return this._httpClient.post<void>('https://63761992b5f0e1eb850298da.mockapi.io/tasks', task);
+  }
 }
